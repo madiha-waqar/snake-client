1006442f86a8ac0a23a3125dffbb531f0e925cfa
madiha-waqar <mail2madihawaqar@gmail.com>
2023-05-18 09:39:14 -0400

    Move Commands

--- client.js
+++ client.js
@@ -5,19 +5,25 @@ const connect = () => {
     host: "172.18.74.140 ",
     port: 50541
   });
-  
-  conn.on("connect", (connect) =>  {
+
+  conn.on("connect", (connect) => {
     console.log("Game On!! You Are Successfully Connected To The Game Server");
-    conn.write("Name: ABC" );
+    conn.write("Name: ABC");
   });
-  
+
+  conn.on("connect", (connect) => {
+    setTimeout(() => {
+      conn.write( "Move: up");
+      }, 2000);
+    });
+
   conn.setEncoding("utf8")
 
- conn.on("data", (data) => {
+  conn.on("data", (data) => {
     console.log("you ded cuz you idled");
   });
-  
+
   return conn;
 }
 
-module.exports = {connect}
+module.exports = { connect }
